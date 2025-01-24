98ec7656617b5b4ff3a7c21f3f5774fd949fa847
Antovigo <antvig@protonmail.com>
2025-01-23 23:16:39 -0500

    long version

--- batch_extract_fims_reads.sh
+++ batch_extract_fims_reads.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -c 4                               # Request 8 cores
-#SBATCH -t 0-00:15                         # Runtime in D-HH:MM format
-#SBATCH -p short                           # Partition to run in
+#SBATCH -t 1-00:00                         # Runtime in D-HH:MM format
+#SBATCH -p medium                           # Partition to run in
 #SBATCH --mem-per-cpu=8G                   # Memory total in GiB
 #SBATCH -o output/%x_%j.out                 # File to which STDOUT will be written
 #SBATCH -e output/%x_%j.err                 # File to which STDERR will be writtenh
